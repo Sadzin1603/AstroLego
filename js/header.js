2a7b3ee82f20584b372f7f949b797faf788ede77
Sadzin1603 <kauanfeliperossi@gmail.com>
2025-10-26 20:21:37 -0300

    Só estilizando um pouco o index

--- js/header.js
+++ js/header.js
@@ -3,7 +3,6 @@ const head2 = document.querySelector('.head2');
 window.addEventListener('scroll', () => {
   
   // Quando o topo do head2 passa da parte superior da tela
-  console.log(window.scrollY)
   if (window.scrollY >= 160 && !head2.classList.contains('fixed')) {
     head2.classList.add('fixed');
   }
