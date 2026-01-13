5ad5aef32d9f7f01c5a5faefcba9c6c101e467d8
Kauan Rossi <164578708+Sadzin1603@users.noreply.github.com>
2026-01-13 15:10:32 -0300

    Responsividade da tela de inicio
    Criei no vercel tbm

--- js/header.js
+++ js/header.js
@@ -1,14 +1,16 @@
 const head2 = document.querySelector('.head2');
 
+// só executa se NÃO for mobile
+const isDesktop = window.matchMedia('(min-width: 768px)');
+
 window.addEventListener('scroll', () => {
-  
-  // Quando o topo do head2 passa da parte superior da tela
+  if (!isDesktop.matches) return; // para no celular
+
   if (window.scrollY >= 160 && !head2.classList.contains('fixed')) {
     head2.classList.add('fixed');
   }
 
-  // Quando ele volta para a tela (ou seja, desce o scroll de volta)
   if (window.scrollY < 100) {
     head2.classList.remove('fixed');
   }
-}); 
+});
