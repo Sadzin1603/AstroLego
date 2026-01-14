f54e7ae3473844de771d6408e962938b4fd51686
Kauan Rossi <164578708+Sadzin1603@users.noreply.github.com>
2026-01-14 03:19:17 -0300

    Responsividade da pagina equipe
    Git, eu irei te vencer um dia seu maldito

--- js/header.js
+++ js/header.js
@@ -1,10 +1,13 @@
 const head2 = document.querySelector('.head2');
 
 // só executa se NÃO for mobile
-const isDesktop = window.matchMedia('(min-width: 768px)');
+const isDesktop = window.matchMedia('(min-width: 1100px)');
 
 window.addEventListener('scroll', () => {
-  if (!isDesktop.matches) return; // para no celular
+  if (!isDesktop.matches){
+    head2.classList.remove('fixed');
+    return; // para no celular
+  } 
 
   if (window.scrollY >= 160 && !head2.classList.contains('fixed')) {
     head2.classList.add('fixed');
